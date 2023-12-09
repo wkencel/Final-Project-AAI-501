d4d163d4f851bd38dcb4f31e177bdd35b8ab8e3e
Will Kencel <wkencel1@gmail.com>
2023-12-09 15:06:11 -0800

    modified random forest model to improve accuracy

--- notebooks/random_forest.py
+++ notebooks/random_forest.py
@@ -37,7 +37,7 @@ encoded_labels = label_encoder.fit_transform(labels)
 X_train, X_test, y_train, y_test = train_test_split(images, encoded_labels, test_size=0.2, random_state=42)
 
 # Initialize the Random Forest Classifier
-rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+rf_classifier = RandomForestClassifier(n_estimators=1000, random_state=42)
 
 # Train the classifier
 rf_classifier.fit(X_train, y_train)
